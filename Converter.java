4541699aff39b83887276e3f5337b8dbe835a30c
Alex <alexanderlinus@gmail.com>
2024-02-16 18:31:47 -0800

    yes

--- Converter.java
+++ Converter.java
@@ -2,7 +2,91 @@ public class Converter {
 
     public static int charToInt(char input)
     {
-        return 1;
+        int x = 1;
+        switch (input)
+        {
+            default:
+                break;
+            case 'a':
+                x = 35;
+                break;
+            case 'b':
+                x = 26;
+                break;
+            case 'c':
+                x = 49;
+                break;
+            case 'd':
+                x = 53;
+                break;
+            case 'e':
+                x = 34;
+                break;
+            case 'f':
+                x = 25;
+                break;
+            case 'g':
+                x = 48;
+                break;
+            case 'h':
+                x = 46;
+                break;
+            case 'i':
+                x = 33;
+                break;
+            case 'j':
+                x = 24;
+                break;
+            case 'k':
+                x = 52;
+                break;
+            case 'l':
+                x = 36;
+                break;
+            case 'm':
+                x = 28;
+                break;
+            case 'n':
+                x = 45;
+                break;
+            case 'o':
+                x = 37;
+                break;
+            case 'p':
+                x = 42;
+                break;
+            case 'q':
+                x = 38;
+                break;
+            case 'r':
+                x = 29;
+                break;
+            case 's':
+                x = 27;
+                break;
+            case 't':
+                x = 43;
+                break;
+            case 'u':
+                x = 39;
+                break;
+            case 'v':
+                x = 32;
+                break;
+            case 'w':
+                x = 22;
+                break;
+            case 'x':
+                x = 23;
+                break;
+            case 'y':
+                x = 44;
+                break;
+            case 'z':
+                x = 47;
+                break;
+        }
+        return x;
     }
 
     public static char intToChar(int input)
@@ -12,3 +96,4 @@ public class Converter {
 
 
 }
+
--- Main.java
+++ Main.java
@@ -1,14 +1,20 @@
+import java.util.Random;
 import java.util.Scanner;
+import java.math.BigInteger;
 public class Main {
 
 public static void main(String[] args)
 {
+    intro();
+    ask();
 
 }
 public static Scanner scan = new Scanner(System.in);
+public static Random rng = new Random();
 public static void intro()
 {
     System.out.println("Welcome to the Number Cipher Encryptor thing. I really like making these, apparently.");
+
 }
 
 public static void ask()
@@ -29,9 +35,33 @@ public static void ask()
     }
 
 }
+public static long generateKey()
+{
+    long min = 10L;
+    long max = 99L;
+    return rng.longs(min, max +1).findFirst().getAsLong();
+}
+
 
 public static void encrypt()
 {
+    long key = generateKey();
+    String encryptedMessage = "";
+    System.out.println("Please input the message you wish to encrypt:");
+    String message = scan.nextLine().toLowerCase();
+    for(int i = 0; i < message.length(); i++)
+    {
+        encryptedMessage += (Converter.charToInt(message.charAt(i)));
+        encryptedMessage += "1";
+    }
+
+    long encryptedLong = Long.parseLong(encryptedMessage);
+    encryptedLong = encryptedLong * key;
+
+    encryptedMessage = "";
+    encryptedMessage += encryptedLong;
+
+    System.out.println("Your Symmetric Key is:" + key + "\nEncrypted Message: \n" + encryptedMessage);
 
 }
 public static void decrypt()
@@ -40,3 +70,39 @@ public static void decrypt()
 }
 
 }
+
+/*
+w22
+x23
+j24
+f25
+b26
+s27
+m28
+r29
+v32
+i33
+e34
+a35
+l36
+o37
+q38
+u39
+p42
+t43
+y44
+n45
+h46
+z47
+g48
+c49
+k52
+d53
+
+ */
+
+
+/*
+
+
+ */
